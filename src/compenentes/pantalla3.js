cf653b024f12c6cece840a124afd8b4b1df56e49
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-04-22 10:54:26 -0500

    publicaciones

--- src/compenentes/pantalla3.js
+++ src/compenentes/pantalla3.js
@@ -23,3 +23,52 @@ salir.addEventListener('click' , pantalla1 )
 let bttninicio = document.querySelector('.inicio');
 bttninicio.addEventListener('click', publicaciones);
 }
+// Hacer publicaciones, eliminación y borrado
+
+let bodyTable= document.querySelector("#bodyTable");
+const boxComment= document.querySelector("#contentComment");
+
+//enviando publicaciones a la base de datos Firestore
+boxComment.addEventListener("submit", (e) =>{
+e.preventDefault(); 
+    db.collection("publicaciones").add({
+        post: boxComment.postShare.value
+    })
+    .then(function(docRef){
+    console.log("Document written with ID: ", docRef.id);
+    boxComment.postShare.value= "";
+    })
+    .catch(function(error){
+    console.log("Error adding document: ", error);
+  });
+
+});
+
+// leer datos /traerlos y pintarlos en intefaz
+db.collection("publicaciones").onSnapshot((querySnapshot)=> {
+    bodyTable.innerHTML= "";
+    querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().post}`);
+    bodyTable.innerHTML += 
+        `
+        <tr>
+        <td>${doc.data().post}</td>
+        <td><button id="delete" class="delete">Eliminar</button></td>
+        <td><button id="editing" class="editing">Editar</button></td>
+        
+        </tr>
+      `; 
+      //listeners
+      //let btnDelete= document.querySelector("#delete");
+      //btnDelete.onclick = ()=>{console.log("mija")}
+  });   
+});
+
+//borrando las publicaciones anteriormente realizadas
+//function deletingPublish(){
+//  db.collection("publicaciones").doc(id).delete().then(function() {
+//      console.log("Document successfully deleted!");
+//  }).catch(function(error) {
+//      console.error("Error removing document: ", error);
+// });
+//}
