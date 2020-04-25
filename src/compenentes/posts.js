a74e333ea4742bd7d625030ddb2d72d941f30512
paola <adripaogui@gmail.com>
2020-04-24 19:59:30 -0500

    like y editar

--- src/compenentes/posts.js
+++ src/compenentes/posts.js
@@ -12,11 +12,14 @@ export const publicaciones = (user) => {
 <h1 class='userName'>${user.displayName}</h1>
 <input type='text' id="txt" class='txt' placeholder='¿Que quieres compartir?'>
 <input type='button' value='Compartir' class='compartir' id="Compartir">
-<input id="sendComment" type='button' value='Guardar' class='Guardar'>
+<input id="sendComment" type='button' value='Guardar' class='guardar'>
 <img class="inputImg" id='file' src="/img/file.png">
 <div id='root1'>
 </div>
 <div id='root2'></div>
+<div id="counter" class="counter">
+      <button id="like"  class="like">Me gusta</button><p class="cont" id="counting"></p> 
+    </div>
 `;
 
 root.innerHTML = post;
@@ -25,7 +28,7 @@ let root2 = document.querySelector('#root2');
 let text = document.querySelector('#txt');
 let addBtn = document.querySelector('.compartir');
 let db = firebase.firestore();
-
+//funcion para enviar posts a base de datos
 addBtn.addEventListener('click', enviar);
 function enviar(){
     db.collection("posts").add({
@@ -38,33 +41,9 @@ function enviar(){
     .catch(function(error) {
         console.error("Error adding document: ", error);
     });
+    document.querySelector('#txt').value = '';
 }
-function editar(id, post){
-   
-        document.querySelector("#txt").value= comment;
-        document.querySelector("#compartir").style.display= "block";
-        document.querySelector("#sendComment").style.display= "none";
-        let editingButton = document.querySelector("Compartir");
-    
-    editingButton.onclick= ()=>{
-        let editingComment = db.collection("pots").doc(id);
-        let newComment= document.querySelector("#txt").value;
-        return editingComment.set({
-        post: newComment
-    })
-        .then(function() {
-        console.log("Document successfully updated!");
-        document.querySelector("#txt").value= "";
-        document.querySelector("#compartir").style.display= "none";
-        document.querySelector("#sendComment").style.display= "block";
-    })
-        .catch(function(error) {
-        // The document probably doesn't exist.
-        console.error("Error updating document: ", error);
-      });
-    
-     }
-}
+
 
 db.collection("posts").onSnapshot((querySnapshot)=> {
     root2.innerHTML ='';
@@ -73,7 +52,7 @@ db.collection("posts").onSnapshot((querySnapshot)=> {
         <p>${doc.data().name}</p>
         <p>${doc.data().texto}</p>
         <button id="delete" value="${doc.id}" class="delete">Eliminar</button>
-        <button  id="edit" class="edit">Editar</button>
+        <button  id="edit" name="${doc.data().texto}" value="${doc.id}" class="edit">Editar</button>
         `
     let nodo = document.createElement('div');
     nodo.classList.add('card')
@@ -87,11 +66,39 @@ let idbttn = document.querySelectorAll('#delete');
         }).catch(function(error) {
           alert("Error removing document: ", error);
         });
-    
-    }
-    ));
-
+}));
+let editbttn = document.querySelectorAll('#edit');
+    editbttn.forEach(btnedit => btnedit.addEventListener('click', (valor, txt) => {
+        txt = btnedit.name; 
+        valor = btnedit.value;     
+          document.querySelector('#txt').value = txt;
+         document.querySelector('.guardar').style.display = 'block';
+        document.querySelector('.compartir').style.display = 'none';
+        let bttnsave = document.querySelector('.guardar');
+        bttnsave.addEventListener('click',function(){
+            let txt = document.querySelector('#txt').value;
+            console.log(txt);
+            let colection= db.collection("pots").doc(valor);
+               colection.update({
+                texto : txt
+               }).then(function(){
+                console.log("documents successfully uptade")
+                text.innerHTML = '';
+                document.querySelector('.guardar').style.display = 'none';
+                document.querySelector('.compartir').style.display = 'block';
+            })
+            .catch(function(error){
+                console.error("Error uptading document: ", error)
+            });
+        })
+    })); 
 });
+
+let likeBttn= document.querySelector('#like');
+let like =0;
+likeBttn.addEventListener('click',()=>{
+    document.querySelector('#counting').innerHTML = ++like;
+})
 
 let salir = document.querySelector('.salir');
 salir.addEventListener('click' , pantalla1 );
